5c9df72eef41b440ba538ea03ecc780a2933e0ca
Alessandro Leyva <josealeyva87@gmail.com>
2017-08-09 09:58:13 -0500

    Getting info from the database

--- server/public/scripts/controllers/phrases.controller.js
+++ server/public/scripts/controllers/phrases.controller.js
@@ -5,41 +5,27 @@ myApp.controller('PhrasesController', function(PhrasesService, UserService) {
   vm.phrasebook = [];
   vm.allPhrase = false;
 
-vm.addToPhrasebook = function() {
-  var newPhrases = {
-    esphrase: vm.esphrase,
-    enphrase: vm.enphrase,
+  vm.getPhrasebook = function() {
+    console.log('in controller, getPhrasebook');
+    PhrasesService.getPhrasebook().then(function(){
+      vm.phrasebook = PhrasesService.data;
+      console.log('back in controller with:', vm.phrasebook);
+    });
+  }; // end getPhrasebook
+
+  vm.deletePhrases = function(phrases){
+    console.log('in delete', phrases);
+    PhrasesService.deletePhrases(phrases._id).then(function(){
+      vm.getPhrasebook();
+    });
+  }; // end deletePhrases
+
+  vm.updatePhrases = function(phrases) {
+    console.log('in update', phrases);
+    PhrasesService.updatePhrases(phrases).then(function(){
+      vm.getPhrasebook();
+    });
   };
-  console.log('in controller sending:', newPhrases);
-  PhrasesService.addToPhrasebook(newPhrases).then(function() {
-    console.log('back in controller after post');
-    vm.getPhrasebook();
-  });
-  vm.esphrase = '';
-  vm.enphrase= '';
-}; // end addToPhrasebook
-
-
-vm.getPhrasebook = function() {
-  console.log('in controller, getPhrasebook');
-  PhrasesService.getPhrasebook().then(function(){
-    vm.phrasebook = PhrasesService.data;
-    console.log('back in controller with:', vm.phrasebook);
-  });
-}; // end getPhrasebook
-
-vm.deletePhrases = function(phrases){
-  console.log('in delete', phrases);
-  PhrasesService.deletePhrases(phrases._id).then(function(){
-    vm.getPhrasebook();
-  });
-}; // end deletePhrases
-
-vm.updatePhrases = function(phrases) {
-  console.log('in update', phrases);
-  PhrasesService.updatePhrases(phrases).then(function(){
-    vm.getPhrasebook();
-  });
-};
+  vm.getPhrasebook();
 
 });
